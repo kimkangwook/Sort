89543af7ba426c2b4f5f90d08a754021bb67fbb0
kangwook <rlarkddnr1686@naver.com>
2023-01-09 08:11:45 +0900

    HeapSort 구현
    heapify : max heap을 만들어주는 함수
    - heapify() : 재귀 호출 방법
    - heapifyWhile() : while 문을 이용한 최적화
    
    재귀호출보다는 while 문을 이용하는 게 효율적임
    
    Main에 Test 코드 작성

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import sort.HeapSort;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{1, 5, 6, 3, 31, 25, 8, 9, 7, 3, 2,48,22,2,1,3,5,1};
+        int[] arr2 = new int[]{1, 5, 6, 3, 31, 25, 8, 9, 7, 3, 2,48,22,2,1,3,5,1};
+        long a = System.nanoTime();
+        HeapSort.heapsort1(arr1);
+        long b = System.nanoTime();
+        long c = System.nanoTime();
+        HeapSort.heapsort2(arr2);
+        long d = System.nanoTime();
+
+        for(int e : arr1) {
+            System.out.println(e);
+        }
+        for(int f : arr2) {
+            System.out.println(f);
+        }
+
+        System.out.println((long)(b-a));
+        System.out.println((long)(d-c));
+
+    }
+}
