a65b1d308ec0c159f5d8ebe4edbb2e71bdfa40bf
kangwook <rlarkddnr1686@naver.com>
2023-01-09 09:17:37 +0900

    MergeSort 구현
    merge : 부분리스트들을 병합해주는 함수
    - merge_sortTopDown() : 부분리스트들을 크기 1까지 나누고 차례로 병합하는 과정
    - merge_sortTopDown() : 부분리스트로 나누고 병합하고 부분리스트의 크기를 늘려 다시 병합하고를 반복하는 과정
    
    Main에 Test 코드 작성

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import sort.HeapSort;
+import sort.MergeSort;
 
 import java.sql.SQLOutput;
 
@@ -7,10 +8,11 @@ public class Main {
         int[] arr1 = new int[]{1, 5, 6, 3, 31, 25, 8, 9, 7, 3, 2,48,22,2,1,3,5,1};
         int[] arr2 = new int[]{1, 5, 6, 3, 31, 25, 8, 9, 7, 3, 2,48,22,2,1,3,5,1};
         long a = System.nanoTime();
-        HeapSort.heapsort1(arr1);
+        MergeSort.merge_sort1(arr1);
         long b = System.nanoTime();
         long c = System.nanoTime();
         HeapSort.heapsort2(arr2);
+        MergeSort.merge_sort2(arr2);
         long d = System.nanoTime();
 
         for(int e : arr1) {
